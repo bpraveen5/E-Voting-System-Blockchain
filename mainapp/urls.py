4eec16610523ec7a3401a5b3543f2ce5d96542ba
B Praveen Kumar <139959868+bpraveen5@users.noreply.github.com>
2024-08-02 16:44:54 +0530

    Add files via upload

---
+++ mainapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views as mainViews
+urlpatterns = [ 
+    path('',mainViews.index, name='index'),
+    path('contact',mainViews.contact, name='contact')
+]
